08e3c3f9b9f66b754fc93cbb088912deb0612d7d
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-28 13:29:09 -0300

    feat: ensure HttpPostClient return httpResponse on axios fail

--- src/infra/axios-http-client/axios-post-client.ts
+++ src/infra/axios-http-client/axios-post-client.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   IHttpClientParams,
   IHttpPostClient
@@ -8,6 +9,13 @@ import axios from 'axios'
 export class AxiosPostClient implements IHttpPostClient {
   async post(params: IHttpClientParams): Promise<HttpResponse> {
     const axiosResponse = await axios.post(params.url, params.body)
+    if (axiosResponse.status !== 200) {
+      const axiosError: any = axiosResponse
+      return Promise.resolve({
+        statusCode: axiosError.response.status,
+        body: axiosError.response.data
+      })
+    }
     return Promise.resolve({
       statusCode: axiosResponse.status,
       body: axiosResponse.data
