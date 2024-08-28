403a34b4d0ad5654d92865014305ade1a10c9d07
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-28 14:16:02 -0300

    refactor: change conditional order

--- src/infra/axios-http-client/axios-post-client.ts
+++ src/infra/axios-http-client/axios-post-client.ts
@@ -9,16 +9,16 @@ import axios from 'axios'
 export class AxiosPostClient implements IHttpPostClient {
   async post(params: IHttpClientParams): Promise<HttpResponse> {
     const axiosResponse = await axios.post(params.url, params.body)
-    if (axiosResponse.status !== 200) {
-      const axiosError: any = axiosResponse
+    if (axiosResponse.status === 200) {
       return Promise.resolve({
-        statusCode: axiosError.response.status,
-        body: axiosError.response.data
+        statusCode: axiosResponse.status,
+        body: axiosResponse.data
       })
     }
+    const axiosError: any = axiosResponse
     return Promise.resolve({
-      statusCode: axiosResponse.status,
-      body: axiosResponse.data
+      statusCode: axiosError.response.status,
+      body: axiosError.response.data
     })
   }
 }
