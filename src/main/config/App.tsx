301e967495d4d04004111ddaeba1f39ee30facbb
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-29 17:46:10 -0300

    feat: ensure react-router-dom redirect to login-page on /login

--- src/main/config/App.tsx
+++ src/main/config/App.tsx
@@ -1,14 +1,10 @@
-import { AxiosPostClient } from '../../infra/axios-http-client/axios-post-client'
-import Login from '../../presentation/pages/login'
-import env from './env'
+import React from 'react'
+import AppRoutes from './routes'
 
 function App() {
   return (
     <>
-      <Login
-        url={env.API_URL || 'any_url'}
-        httpPostClient={new AxiosPostClient()}
-      />
+      <AppRoutes/>
     </>
   )
 }
---
+++ src/main/factory/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Login from "../../../presentation/pages/login"
+import env from '../../config/env'
+import { AxiosPostClient } from '../../../infra/axios-http-client/axios-post-client'
+
+export const makeLoginPage = (): React.JSX.Element => {
+  return <Login
+    url={env.API_URL || 'any_url'}
+    httpPostClient={new AxiosPostClient()}
+  />
+}
