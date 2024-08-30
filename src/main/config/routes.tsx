8268ec88a391d139128afcf3cd7ea569ee6712bb
Daniel <danielfalcaoviktorio@gmail.com>
2024-08-30 14:54:33 -0300

    feat: sign up route add

--- src/main/config/routes.tsx
+++ src/main/config/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter, Outlet, Navigate } from 'react-router-dom'
 import { makeLoginPage } from '../factory/pages/login'
+import { makeSignUpPage } from '../factory/pages/signup'
 import AuthRoutes from '../middleware/auth-routes'
 import make404Page from '../factory/pages/404'
 
@@ -10,6 +11,7 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={makeLoginPage()}/>
+        <Route path='/signup' element={makeSignUpPage()} />
         <Route path='/book' element={<AuthRoutes><div><h1>hello mundo</h1><Outlet/></div></AuthRoutes>}>
           <Route path='/book/cadastrar' element={<h1>Cadastro</h1>}/>
           <Route path='/book/atualizar' element={<h1>Atualização</h1>}/>
