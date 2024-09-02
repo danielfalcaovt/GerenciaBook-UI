e4ac05fab67d234bd7e0efb07e10bbd090349d70
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-02 16:07:43 -0300

    feat: ensure app provides http client context to routes

--- src/main/config/App.tsx
+++ src/main/config/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AppRoutes from './routes'
+import { HttpClientContext } from '../context/data-context'
+import { IHttpDeleteClient } from '../../data/protocols/http/delete/http-delete-client'
+import { IHttpGetClient } from '../../data/protocols/http/get/http-get-client'
+import { IHttpPatchClient } from '../../data/protocols/http/patch/http-patch-client'
+import { IHttpPostClient } from '../../data/protocols/http/post/http-post-client'
 
 function App() {
+  const [HttpClient, setHttpClient] = useState<IHttpDeleteClient | IHttpGetClient | IHttpPatchClient | IHttpPostClient | undefined>(undefined)
+
   return (
     <>
-      <AppRoutes/>
+      <HttpClientContext.Provider value={{ HttpClient, setHttpClient }}>
+        <AppRoutes/>
+      </HttpClientContext.Provider>
     </>
   )
 }
