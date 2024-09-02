c610afe1ae340160eb200add4f795ce8805e2c03
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-02 16:15:26 -0300

    feat: ensure app provides data context to whole routes

--- src/main/config/App.tsx
+++ src/main/config/App.tsx
@@ -5,14 +5,18 @@ import { IHttpDeleteClient } from '../../data/protocols/http/delete/http-delete-
 import { IHttpGetClient } from '../../data/protocols/http/get/http-get-client'
 import { IHttpPatchClient } from '../../data/protocols/http/patch/http-patch-client'
 import { IHttpPostClient } from '../../data/protocols/http/post/http-post-client'
+import { DataContext } from '../context/data-context'
 
 function App() {
   const [HttpClient, setHttpClient] = useState<IHttpDeleteClient | IHttpGetClient | IHttpPatchClient | IHttpPostClient | undefined>(undefined)
+  const [data, setData] = useState(DataContext)
 
   return (
     <>
       <HttpClientContext.Provider value={{ HttpClient, setHttpClient }}>
-        <AppRoutes/>
+        <DataContext.Provider value={{ data, setData }}>
+          <AppRoutes/>
+        </DataContext.Provider>
       </HttpClientContext.Provider>
     </>
   )
