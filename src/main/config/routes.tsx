e2f07853104832f94d244e3d5eb35f311e806e95
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-02 16:13:15 -0300

    refactor: ensure boko route call makebookpage factory

--- src/main/config/routes.tsx
+++ src/main/config/routes.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { Route, Routes, BrowserRouter, Outlet, Navigate } from 'react-router-dom'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
 import { makeLoginPage } from '../factory/pages/login'
 import { makeSignUpPage } from '../factory/pages/signup'
 import AuthRoutes from '../middleware/auth-routes'
 import make404Page from '../factory/pages/404'
+import { makeBookPage } from '../factory/pages/book'
+import PostBookForm from '../../presentation/pages/book/post/post-book-form'
 
 export default function AppRoutes() {
-
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={makeLoginPage()}/>
         <Route path='/signup' element={makeSignUpPage()} />
-        <Route path='/book' element={<AuthRoutes><div><h1>hello mundo</h1><Outlet/></div></AuthRoutes>}>
-          <Route path='/book/cadastrar' element={<h1>Cadastro</h1>}/>
+        <Route path='/book' element={<AuthRoutes>{makeBookPage()}</AuthRoutes>}>
+          <Route path='/book/cadastrar' element={<PostBookForm/>}/>
           <Route path='/book/atualizar' element={<h1>Atualização</h1>}/>
           <Route path='/book/remover' element={<h1>Remoção</h1>}/>
           <Route path='/book/consultar' element={<h1>Consulta</h1>}/>
