9adc45410c7e95c7ccca06cc004f8694b9ef3fb0
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-03 16:27:27 -0300

    feat:  ensure AxiosGetClient return httpResponse on axios succeed

--- src/infra/axios-http-client/get/axios-get-client.ts
+++ src/infra/axios-http-client/get/axios-get-client.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IHttpGetClient } from "../../../data/protocols/http/get/http-get-client";
 import { IHttpClientParams } from "../../../data/protocols/http/post/http-post-client";
 import { HttpResponse } from "../../../presentation/protocols/http";
@@ -5,7 +6,18 @@ import axios from "axios";
 
 export class AxiosGetClient implements IHttpGetClient {
   async get(params: IHttpClientParams): Promise<HttpResponse> {
-    await axios.get(params.url)
-    return new Promise(resolve => resolve({statusCode: 200}))
+    const axiosResponse = await axios.get(params.url)
+    if (axiosResponse.status === 200) {
+      return new Promise(resolve => resolve({
+        statusCode: axiosResponse.status,
+        body: axiosResponse.data
+      }))
+    } else {
+      const axiosError: any = axiosResponse
+      return new Promise(resolve => resolve({
+        statusCode: axiosError.response.status,
+        body: axiosError.response.data
+      }))
+    }
   }
 }
