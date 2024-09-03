48f31e900edcf4679f6010b92a1f09a6e752f690
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-03 13:59:21 -0300

    feat: ensure GetToken return getItem result

--- src/data/usecases/token-getter/get-token.ts
+++ src/data/usecases/token-getter/get-token.ts
@@ -4,7 +4,7 @@ import { ItemStorage } from "../../protocols/token/item-storage";
 export class GetToken implements IGetToken {
   constructor(private readonly itemStorage: ItemStorage) {}
   getToken(key: string): string {
-    this.itemStorage.getItem(key)
-    return ''
+    const result = this.itemStorage.getItem(key)
+    return result
   }
 }
