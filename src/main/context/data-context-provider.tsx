7fe6d342b7c4fbf3d1f0b9dba6e88db4dfd473db
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-03 17:02:00 -0300

    feat: ensure DataContextProvider get data before app render the components

--- src/main/context/data-context-provider.tsx
+++ src/main/context/data-context-provider.tsx
@@ -1,13 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { DataContext } from './data-context'
 import Loader from '../../presentation/components/loader'
+import { AxiosGetClient } from '../../infra/axios-http-client/get/axios-get-client'
+import { LocalStorageRepository } from '../../infra/local-storage/local-storage'
+import env from '../config/env'
+import { HttpResponse } from '../../presentation/protocols/http'
 
 export default function DataContextProvider({ children }: any) {
   const [data, setData] = useState({})
   const [loading, setLoading] = useState(false)
-  
+
+  useEffect(() => {
+    const localStorageRepo = new LocalStorageRepository()
+    const token = localStorageRepo.getItem('token')
+    setLoading(true)
+    const httpClient = new AxiosGetClient(token ? token : undefined)
+    httpClient.get({ url: env.API_URL + '/books' })
+      .then((result: HttpResponse) => {
+        setData((oldValue: any) => ({
+          ...oldValue,
+          books: result.body
+        }))
+      })
+      .catch((err: any) => {
+        console.log(err)
+        setLoading(false)
+      })
+      .finally(() => {
+        console.log('fim')
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <>
       {loading && <Loader />}
