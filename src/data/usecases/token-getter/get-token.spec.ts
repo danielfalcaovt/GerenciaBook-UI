833f1970950f2307f3c3f8000ba9849c8c8ebdc0
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-03 13:51:57 -0300

    test: ensure GetToken call getItem with correct value

---
+++ src/data/usecases/token-getter/get-token.spec.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ItemStorage } from "../../protocols/token/item-storage"
+import { GetToken } from "./get-token"
+
+interface SutTypes {
+  sut: GetToken
+  getTokenStub: ItemStorage
+} 
+
+const makeSut = (): SutTypes => {
+  const getTokenStub = makeGetTokenStub()
+  const sut = new GetToken(getTokenStub)
+  return {
+    sut,
+    getTokenStub
+  }
+}
+
+const makeGetTokenStub = (): ItemStorage => {
+  class GetTokenStub implements ItemStorage {
+    getItem(item: string): string {
+      return 'any_token'
+    }
+  }
+  return new GetTokenStub()
+}
+
+describe('TokenGetter', () => {
+  it('Should call getItem with correct value', () => {
+    const { sut, getTokenStub } = makeSut()
+    const getSpy = jest.spyOn(getTokenStub, 'getItem')
+    sut.getToken('token')
+    expect(getSpy).toHaveBeenCalledWith('token')
+  })
+})
