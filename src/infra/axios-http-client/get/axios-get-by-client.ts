bbcd221ecb90f6baaad47b624f6a67dceaee0bec
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-04 13:58:59 -0300

    feat: ensure AxiosGetByClient return httpResponse on axios fails

--- src/infra/axios-http-client/get/axios-get-by-client.ts
+++ src/infra/axios-http-client/get/axios-get-by-client.ts
@@ -1,30 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios";
-import { IHttpGetByClient } from "../../../data/protocols/http/get/http-get-by-client";
-import { IHttpClientParams } from "../../../data/protocols/http/post/http-post-client";
-import { HttpResponse } from "../../../presentation/protocols/http";
+import axios from 'axios'
+import { IHttpGetByClient } from '../../../data/protocols/http/get/http-get-by-client'
+import { IHttpClientParams } from '../../../data/protocols/http/post/http-post-client'
+import { HttpResponse } from '../../../presentation/protocols/http'
 
 export class AxiosGetByClient implements IHttpGetByClient {
   async getBy(params: IHttpClientParams): Promise<HttpResponse> {
     let url = params.url
     url += '?'
-    for (const pos of ['book_name', 'student_name', 'student_class', 'lend_day']) {
+    for (const pos of [
+      'book_name',
+      'student_name',
+      'student_class',
+      'lend_day'
+    ]) {
       if (params.body && params.body[pos]) {
         url += `${pos}=${params.body[pos]}`
       }
     }
-    const axiosResponse = await axios.get(url)
-    if (axiosResponse.status === 200) {
-      return new Promise(resolve => resolve({
-        statusCode: axiosResponse.status,
-        body: axiosResponse.data
-      }))
-    } else {
-      const axiosError: any = axiosResponse
-      return new Promise(resolve => resolve({
-        statusCode: axiosError.response.status,
-        body: axiosError.response.data
-      }))
+    try {
+      const axiosResponse = await axios.get(url)
+      return new Promise((resolve) =>
+        resolve({
+          statusCode: axiosResponse.status,
+          body: axiosResponse.data
+        })
+      )
+    } catch (err: any) {
+      const axiosError = err
+      return new Promise((resolve) =>
+        resolve({
+          statusCode: axiosError.response.status,
+          body: axiosError.response.data
+        })
+      )
     }
   }
 }
