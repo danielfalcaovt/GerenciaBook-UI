5368fcd445d816c085f913a5a30768943e2880b2
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-05 16:14:04 -0300

    test: ensure AxiosUpdateClient return httpResponse on axios succeed

--- src/infra/axios-http-client/update/axios-update-client.test.ts
+++ src/infra/axios-http-client/update/axios-update-client.test.ts
@@ -7,7 +7,7 @@ import { AxiosUpdateClient } from './axios-update-client'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
-mockedAxios.get.mockResolvedValue({ status: 200, data: [] } as any)
+mockedAxios.patch.mockResolvedValue({ status: 200, data: [] } as any)
 
 const makeFakeRequest = (): IHttpClientParams => ({
   url: faker.internet.url(),
@@ -22,6 +22,12 @@ describe('AxiosUpdateClient', () => {
     const patchSpy = jest.spyOn(mockedAxios, 'patch')
     const expectedValue = makeFakeRequest()
     await sut.patch(expectedValue)
-    expect(patchSpy).toHaveBeenCalledWith(expectedValue.url, expectedValue.body)
+    expect(patchSpy).toHaveBeenCalledWith(expectedValue.url, expectedValue.body, {})
+  })
+  it('Should return httpResponse on axios succeed', async () => {
+    const sut = new AxiosUpdateClient()
+    const response = await sut.patch(makeFakeRequest()) 
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual([])
   })
 })
