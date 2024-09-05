4e4ee119a8f47a339aaa52f1c00172b0b3402b05
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-05 18:44:03 -0300

    feat: aside redirect buttons and style add

---
+++ src/presentation/components/aside.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Aside() {
+  return (
+    <aside>
+      <div id="aside-header">
+        <img
+          src="/assets/pages/login/gerenciabook.png"
+          alt="livro acima de um texto escrito gerencia book"
+        ></img>
+      </div>
+      <div id="link-container">
+        <ul id="link-main">
+          <li>
+            <Link to={'/book/cadastrar'}>
+              <div>
+                <img
+                  src="/assets/pages/books/add-book.png"
+                  alt="botão para adicionar novo empréstimo"
+                />
+              </div>
+              <p>Emprestar</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={'/book/atualizar'}>
+              <div>
+                <img
+                  src="/assets/pages/books/patch-book.png"
+                  alt="botão para atualizar um empréstimo"
+                />
+              </div>
+              <p>Alterar</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={'/book/consultar'}>
+              <div>
+                <img
+                  src="/assets/pages/books/search-book.png"
+                  alt="botão para buscar um empréstimo"
+                />
+              </div>
+              <p>Consultar</p>
+            </Link>
+          </li>
+          <li>
+            <Link to={'/book/remover'}>
+              <div>
+                <img
+                  src="/assets/pages/books/delete-book.png"
+                  alt="botão para confirmar devolução"
+                />
+              </div>
+              <p>Devolução</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  )
+}
