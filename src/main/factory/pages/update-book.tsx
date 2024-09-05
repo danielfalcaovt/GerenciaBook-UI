86397312800f1b0960ae4e7a6114635b0332668a
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-05 16:28:27 -0300

    feat: UpdateBookForm factory creation

---
+++ src/main/factory/pages/update-book.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react"
+import env from '../../config/env'
+import UpdateBookForm from "../../../presentation/pages/book/update/update-book-form"
+import { LocalStorageRepository } from "../../../infra/local-storage/local-storage"
+import { DataContext } from "../../context/data-context"
+import { AxiosUpdateClient } from "../../../infra/axios-http-client/update/axios-update-client"
+
+export const makeGetBookPage = (): React.JSX.Element => {
+  const itemStorage = new LocalStorageRepository()
+  const token = itemStorage.getItem('token')
+  return <UpdateBookForm
+    url={env.API_URL + '/books'}
+    HttpClient={new AxiosUpdateClient(token ? token : undefined)}
+    context={DataContext}
+  />
+}
