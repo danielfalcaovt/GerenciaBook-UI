a3c64e3919f75de286c3c35865ced27ffaacf84f
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-06 17:15:29 -0300

    feat: ensure RemoteGetBook return book array on getBy succeed

--- src/data/usecases/book/get/get-book.ts
+++ src/data/usecases/book/get/get-book.ts
@@ -1,3 +1,5 @@
+import { InvalidParamsError } from "../../../../domain/errors/invalid-params-error";
+import { UnexpectedError } from "../../../../domain/errors/unexpected-error";
 import { IBook } from "../../../../domain/protocols/book/book";
 import { IGetBook, IGetBookModel } from "../../../../domain/usecases/book/iget-book";
 import { IHttpGetByClient } from "../../../protocols/http/get/http-get-by-client";
@@ -8,7 +10,17 @@ export class RemoteGetBook implements IGetBook {
     private readonly httpClient: IHttpGetByClient
   ) {}
   async getBy(book: IGetBookModel): Promise<IBook[]> {
-    await this.httpClient.getBy({ url: this.url, body: book })
-    return Promise.resolve([])
+    const response = await this.httpClient.getBy({ url: this.url, body: book })
+    switch (response.statusCode) {
+      case 200:
+        return Promise.resolve(response.body)
+        break
+      case 400:
+        throw new InvalidParamsError()
+        break
+      default:
+        throw new UnexpectedError()
+        break
+    }
   }
 }
