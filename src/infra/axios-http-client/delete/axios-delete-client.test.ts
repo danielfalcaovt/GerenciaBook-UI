1e569b6f0d3bbfe357b42b1dad9fa2c7f67bde64
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-06 13:51:48 -0300

    test: ensure AxiosDeleteClient call axios with correct values

---
+++ src/infra/axios-http-client/delete/axios-delete-client.test.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IHttpClientParams } from "../../../data/protocols/http/post/http-post-client"
+import * as faker from 'faker'
+import { AxiosDeleteClient } from "./axios-delete-client"
+import axios from "axios"
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+mockedAxios.get.mockResolvedValue({ status: 200, data: [] } as any)
+
+const makeFakeRequest = (): IHttpClientParams => ({
+  url: faker.internet.url(),
+  body: {
+    id: 'any_id'
+  }
+})
+
+describe('AxiosDeleteClient', () => {
+  it('Should call axios with correct values', async () => {
+    const sut = new AxiosDeleteClient()
+    const deleteSpy = jest.spyOn(mockedAxios, 'delete')
+    const expectedValue = makeFakeRequest()
+    await sut.delete(expectedValue)
+    const expectedUrl = expectedValue.url + `?id=${expectedValue.body.id}`
+    expect(deleteSpy).toHaveBeenCalledWith(expectedUrl, {})
+  })
+})
