341245343e3c4dda655380d30130fd34575faaf1
Daniel <danielfalcaoviktorio@gmail.com>
2024-09-09 14:20:57 -0300

    feat: move routes creation to a factory

--- src/main/config/routes.tsx
+++ src/main/config/routes.tsx
@@ -1,26 +1,13 @@
-import React from 'react'
-import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
-import AuthRoutes from '../middleware/auth-routes'
-import make404Page from '../factory/pages/error/404'
-import { makeLoginPage, makeSignUpPage, makeBookPage, makePostBookPage, makeGetBookPage, makeUpdateBookPage } from './config-protocols'
-import { makeDeleteBookPage } from '../factory/components/form/delete-book'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/app-routes'
+import DataContextProvider from '../context/data-context-provider'
 
-export default function AppRoutes() {
-  
+export default function MakeRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={makeLoginPage()}/>
-        <Route path='/signup' element={makeSignUpPage()} />
-        <Route path='/book' element={<AuthRoutes>{makeBookPage()}</AuthRoutes>}>
-          <Route path='/book/cadastrar' element={makePostBookPage()}/>
-          <Route path='/book/atualizar' element={makeUpdateBookPage()}/>
-          <Route path='/book/devolver' element={makeDeleteBookPage()}/>
-          <Route path='/book/consultar' element={makeGetBookPage()}/>
-        </Route>
-        <Route path='/' element={<AuthRoutes><Navigate to='/book'/></AuthRoutes>} />
-        <Route path='*' element={make404Page()} />
-      </Routes>
+      <DataContextProvider>
+        <AppRoutes />
+      </DataContextProvider>
     </BrowserRouter>
   )
 }
